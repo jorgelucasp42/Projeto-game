420be3ee127c8d46d8df63cce43473286122ac78
Jorge <103595844+jorgelucasp42@users.noreply.github.com>
2024-01-20 01:38:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ let numeroSecreto = 29;
 let chute = prompt('Escolha um número entre 1 e 30');
 
 if(chute == numeroSecreto){
-    console.log('Isso aí! Você descobriu o número secreto! (29)')
-}
+    console.log(`Isso aí! Você descobriu o número secreto !`)
+}else{
+    alert('Você errou!')
+} 
