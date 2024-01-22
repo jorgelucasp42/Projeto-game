8dfcb09d66a852db5b86e71f352c738d981c7ed6
Jorge <103595844+jorgelucasp42@users.noreply.github.com>
2024-01-22 01:43:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 alert('Bem vindo ao jogo do número secreto!');
-let numeroSecreto = 4;
-let chute = prompt('Escolha um número entre 1 e 10');
+let numeroSecreto = parseInt(Math.random()*10+1);
+console.log(numeroSecreto);
+//alert(`O número secreto é ${numeroSecreto}!`);
+let chute;
+let tentativas=1;
 
-if(chute == numeroSecreto){
-    console.log(`Isso aí! Você descobriu o número secreto ${numerosecreto}!`)
-}else{
-    alert('Você errou!')
-} 
+while (chute != numeroSecreto) {
+    chute = parseInt(prompt('Escolha um número entre 1 e 10'));
+
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}!`);
+        } else {
+            alert(`O número secreto é maior que ${chute}!`);
+
+        }
+        tentativas++; 
+    }
+   
+}
+
+let palavraTentativa = tentativas >1? 'tentativas':'tentativa'; 
+
+alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} em ${tentativas} ${palavraTentativa}!`);
+
+
+
